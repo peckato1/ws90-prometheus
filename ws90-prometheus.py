e418ca68429b155638e66c7d02caa06dc5d31000
Tomáš Pecka <peckato1@fit.cvut.cz>
2026-01-17 22:14:26 +0100

    victoriametrics import

--- ws90-prometheus.py
+++ ws90-prometheus.py
@@ -7,6 +7,7 @@ Usage:
     ws90-prometheus.py
         [--id=<id>]...
         [--port=<port>]
+        [--vmbaseurl=<url>]
         [--clear=<clear>]
         [--log=<systemd|stderr>]
         [--log-level=<level>]
@@ -16,6 +17,7 @@ Usage:
 Options:
     --id=<id>               Device ID. Can be decimal or hex (prefix with 0x). Can specify multiple times. If not specified, all devices are being monitored.
     --port=<port>           Port to listen on [default: 8000]
+    --vmbaseurl=<url>       VictoriaMetrics base URL. If not specified, VictoriaMetrics publishing is disabled.
     --log-level=<level>     Log level (debug, info, warning, error) [default: info]
     --clear=<clear>         Remove metrics for device after <clear> seconds. 0 for never. Useful for purging outdated metrics when the receiver stops receiving new data for a device. [default: 120]
     --log=<systemd|stderr>  Log to systemd journal or stderr [default: stderr]
@@ -26,12 +28,15 @@ Options:
 import asyncio
 import blinker
 import concurrent.futures
+import datetime
 import docopt
 import logging
 import json
 import prometheus_client as prom
+import requests
 import sys
 import threading
+import urllib.parse
 
 
 logger = logging.getLogger(__name__)
@@ -140,6 +145,55 @@ class PrometheusPublisher:
             m.remove(device_id)
         for _, m in self.model_info.items():
             m.remove(device_id)
+
+
+class VictoriaMetricsPublisher:
+    def __init__(self, vmbaseurl, metrics_data, model_data):
+        self.vmbaseurl = vmbaseurl
+        self.metrics_data = metrics_data
+        self.model_data = model_data
+
+    def _post(self, device_id, data, format):
+        resp = requests.post(
+            urllib.parse.urljoin(self.vmbaseurl, "/api/v1/import/csv"),
+            params={
+                "format": ",".join(format),
+                "extra_label": f"id={device_id}",
+            },
+            data=",".join(map(str, data)).strip(),
+        )
+        resp.raise_for_status()
+        logger.debug(f"ws90: Posted data to VictoriaMetrics for device {device_id}: {data} (response: {resp.status_code})")
+
+    def _construct_metrics(self, data, device_id, dt):
+        columns = ["1:time:unix_s"]
+        csv_line = [int(dt.timestamp())]
+
+        for i, (json_key, name, _, postprocess) in enumerate(self.metrics_data, 2):
+            value = data[json_key] if postprocess is None else postprocess(data[json_key])
+            columns.append(f"{i}:metric:{name}")
+            csv_line.append(value)
+            break
+
+        return csv_line, columns
+
+    def _construct_modelinfo(self, data, device_id, dt, name, keys):
+        columns = ["1:time:unix_s", f"2:metric:{name}_info"]
+        csv_line = [int(dt.timestamp()), 1]
+
+        for i, key in enumerate(keys, 3):
+            columns.append(f"{i}:label:{name}")
+            csv_line.append(data[key])
+
+        return csv_line, columns
+
+    def data_callback(self, data):
+        device_id = data["id"]
+        dt = datetime.datetime.strptime(data["time"], "%Y-%m-%d %H:%M:%S")
+
+        self._post(device_id, *self._construct_metrics(data, device_id, dt))
+        for name, _, keys in self.model_data:
+            self._post(device_id, *self._construct_modelinfo(data, device_id, dt, name, keys))
 
 
 class WS90Reader(threading.Thread):
@@ -265,6 +319,10 @@ if __name__ == "__main__":
 
     p = PrometheusPublisher(int(args["--clear"]), FIELDS, FIELDS_MODEL)
     sig.connect(p.data_callback)
+
+    if args["--vmbaseurl"] is not None:
+        v = VictoriaMetricsPublisher(args["--vmbaseurl"], FIELDS, FIELDS_MODEL)
+        sig.connect(v.data_callback)
 
     exc_watcher = concurrent.futures.Future()
     thr_reader = WS90Reader(args["--cmd"], list(map(lambda x: as_number(x, allow_hex=True), args["--id"])), sig, exc_watcher)
